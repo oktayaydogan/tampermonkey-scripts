76142142a0b4280594a84a27a38e31072af2c27b
Nurullah Akın <nurullah.at.work@outlook.com>
2025-06-03 16:44:55 +0300

    init: show issue progress.js

---
+++ Show issue progress.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Show issue progress
+// @namespace    https://github.com/nurullahakin
+// @version      2025-06-03
+// @description  Show task progress in issues
+// @author       Noreh AD
+// @match        https://github.com/*/*/issues/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict";
+})();
